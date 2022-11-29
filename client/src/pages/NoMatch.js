8f9ca4ac2f03c18ee190c59003fbcfecc2ab9b6d
Nilu <nirali.rkapadia@gmail.com>
2022-11-29 15:02:59 -0800

    Adds a new client folder

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Message from "../components/Message";
+
+const NoMatch = () => {
+  return (
+    <div>
+      <Message>
+        <h1>404 Page Not Found</h1>
+  
+      </Message>
+    </div>
+  );
+};
+
+export default NoMatch;
