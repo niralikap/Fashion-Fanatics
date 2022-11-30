d6698eef495e61634b0b235706a9bd3dfec1d8ee
Nilu <nirali.rkapadia@gmail.com>
2022-11-29 17:27:04 -0800

    Adds the routes for the cart, order, checkout and payments along with the controllers and apis in the utils folder from Fashion Freaks repository

---
+++ server/routes/api/product-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+    get_items,
+    post_item,
+    update_item,
+    delete_item,
+  } = require('../../controllers/productControllers');
+
+
+router.route('/').get(get_items);
+router.route('/').post(post_item);
+router.route('/:id').put(update_item);
+router.route('/:id').delete(delete_item);
+
+
+module.exports = router;
