42a7cac9f6c9be6f378fe27c68515f9d9c1f77b7
Nilu <nirali.rkapadia@gmail.com>
2022-12-01 20:20:07 -0800

    Updates routes

--- server/routes/api/order-routes.js
+++ server/routes/api/order-routes.js
@@ -1,11 +1,11 @@
 
-const router = require('express').Router();
-const {
-    get_orders,
-    checkout,
-  } = require('../../controllers/orderControllers');
+// const router = require('express').Router();
+// const {
+//     get_orders,
+//     checkout,
+//   } = require('../../controllers/orderControllers');
 
-router.route('/order/:id').get(get_orders);
-router.route('/order/:id').post(checkout);
+// router.route('/order/:id').get(get_orders);
+// router.route('/order/:id').post(checkout);
 
-module.exports = router;
+// module.exports = router;
