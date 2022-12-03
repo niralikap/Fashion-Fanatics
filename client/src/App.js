2cf4291f04d7ac062b41d927b1bccffe420d3e82
Nilu <nirali.rkapadia@gmail.com>
2022-12-03 12:37:45 -0800

    Updates the port link in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import OrderHistory from './pages/OrderHistory';
 import Header from './pages/Header';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
